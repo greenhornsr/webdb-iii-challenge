a28498b47dc16ddfc9e49f6190d0e18be119a7e0
greenhornsr <srollins11279@gmail.com>
2019-06-05 14:13:24 -0700

    added students table

---
+++ migrations/20190605135606_add_students_table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('students', function(tbl) {
+        tbl.increments();  // adds primary key that auto-increments
+
+        tbl
+        .string('name', 255)
+        .notNullable()
+
+        tbl
+        .integer('cohort_id')
+        .unsigned()
+        .references('id')
+        .inTable('cohorts')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+        tbl
+        .timestamp('createdAt')
+        .defaultTo(knex.fn.now());
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('students');
+};
